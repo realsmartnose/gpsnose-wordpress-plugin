d9c81dda5e7bb437423109da002940249c9858ed
Juergen Furrer <juergen.furrer@gmail.com>
2023-09-25 13:41:15 +0200

    Update lisence

--- gpsnose.php
+++ gpsnose.php
@@ -1,34 +1,83 @@
 <?php
 
 /**
- * Plugin Name: GpsNose
- * Plugin URI:  https://www.gpsnose.com/
- * Description: Extension to integrate GpsNose into WordPress - GpsNose connects anonymous people to the real world using the web and mobiles
- * Version:     1.0.0
- * Author:      Juergen Furrer
- * Author URI:  https://www.gpsnose.com/
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/realsmartnose
+ * @since             1.0.0
+ * @package           Gpsnose
+ *
+ * @wordpress-plugin
+ * Plugin Name:       GpsNose
+ * Plugin URI:        https://www.gpsnose.com
+ * Description:       GpsNose connects anonymous people to the real world using the web and mobiles
+ * Version:           1.0.0
+ * Author:            smart.nose
+ * Author URI:        https://github.com/realsmartnose
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       gpsnose
+ * Domain Path:       /languages
  */
 
-require "vendor/autoload.php";
+// If this file is called directly, abort.
+if (! defined('WPINC')) {
+    die();
+}
 
-use SmartNoses\Gpsnose\Backend\Settings;
-use SmartNoses\Gpsnose\Frontend\Header;
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define('GPSNOSE_VERSION', '1.0.0');
 
-$settings = new Settings('gpsnose');
-$settings->admin_menu();
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-gpsnose-activator.php
+ */
+function activate_gpsnose()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-gpsnose-activator.php';
+    Gpsnose_Activator::activate();
+}
 
-$header = new Header();
-$header->set_meta();
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-gpsnose-deactivator.php
+ */
+function deactivate_gpsnose()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-gpsnose-deactivator.php';
+    Gpsnose_Deactivator::deactivate();
+}
 
+register_activation_hook(__FILE__, 'activate_gpsnose');
+register_deactivation_hook(__FILE__, 'deactivate_gpsnose');
 
-/*
-$debug_tags = [];
-add_action('all', function ($tag) {
-    global $debug_tags;
-    if (in_array($tag, $debug_tags)) {
-        return;
-    }
-    $debug_tags[] = $tag;
-    echo "<pre style=\"color:red\">{$tag}</pre>";
-});
-*/
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path(__FILE__) . 'includes/class-gpsnose.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since 1.0.0
+ */
+function run_gpsnose()
+{
+    $plugin = new Gpsnose();
+    $plugin->run();
+}
+run_gpsnose();
